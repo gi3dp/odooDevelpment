6f71d90f614c5671f400f709b08b4f7c346e4141
Goya- <zongyang3@gmail.com>
2016-09-26 22:53:50 +0800

    fix bug

--- school/__openerp__.py
+++ school/__openerp__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "school",
+    'name': "School Check",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        用于学校验收
     """,
 
     'author': "My Company",
@@ -26,7 +26,6 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- school/models/models.py
+++ school/models/models.py
@@ -2,7 +2,7 @@
 
 from openerp import models, fields, api
 
-class school(models.Model):
+class School(models.Model):
     _name = 'school.school'
 
 
@@ -13,7 +13,7 @@ class school(models.Model):
     x_phoneNumber = fields.Char(string="电话")
 
     x_trainingteacher = fields.Char(string="培训老师")
-    x_receptionData = fields.Date(string="接受日期")
+    x_receptionData = fields.Date(string="接收日期")
     x_contact2= fields.Char(string="联系人2")
     x_phoneNumber2 = fields.Char(string="电话2")
     x_remark = fields.Char(string="备注")
@@ -24,12 +24,4 @@ class school(models.Model):
     x_equipmentNumber = fields.Integer(required=False, string="设备套数")
     x_schoolAddress = fields.Char(required=False, string="学校地址")
     x_Name = fields.Char(required=False, string="联系人")
-    x_contrctPhoneNumber = fields.Char(required=False, string="联系人电话(座机或手机)")
-
-
-
-    description = fields.Text()
-
-    @api.depends('value')
-    def _value_pc(self):
-        self.value2 = float(self.value) / 100
+    x_contractPhoneNumber = fields.Char(required=False, string="联系人电话(座机或手机)")
