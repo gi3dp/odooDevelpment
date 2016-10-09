cf7753e5443549d96cca57cbed13b59226aa8e17
zong yang <you@example.com>
2016-10-09 21:28:22 +0800

    - [x]1. 修改搜索视图，添加seperate，自定义筛选未验收的

--- school/models/models.py
+++ school/models/models.py
@@ -2,9 +2,9 @@
 
 from openerp import models, fields, api
 
+
 class School(models.Model):
     _name = 'school.school'
-
 
     x_checkCondition = fields.Boolean(string="验收情况")
     x_checkCertificationNumber = fields.Integer(string="验收单张数")
@@ -18,6 +18,7 @@ class School(models.Model):
     x_phoneNumber2 = fields.Char(string="电话2")
     x_remark = fields.Char(string="备注")
     x_city = fields.Char(required=False, string="市，州")
+
     x_town = fields.Char(required=False, string="县区")
     x_QQ = fields.Char(required=False, string="QQ号")
     x_mail = fields.Char(string="邮箱地址")
@@ -25,3 +26,10 @@ class School(models.Model):
     x_schoolAddress = fields.Char(required=False, string="学校地址")
     x_Name = fields.Char(required=False, string="联系人")
     x_contractPhoneNumber = fields.Char(required=False, string="联系人电话(座机或手机)")
+
+    @api.onchange('x_receptionData')
+    def _take_cerfication_number(self):
+        for r in self:
+            if self.x_receptionData:
+                r.x_checkCertificationNumber = r.x_equipmentNumber
+                r.x_checkCondition = True
