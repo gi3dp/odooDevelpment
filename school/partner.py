f99da0f7f46e571e7a9b59464b0d8a815d3be80e
Goya- <zongyang3@gmail.com>
2016-11-14 15:20:11 +0800

    精简售后学校信息tree视图

--- school/partner.py
+++ school/partner.py
@@ -7,8 +7,9 @@ class ResPartner(models.Model):
     is_eiec = fields.Boolean("是电教馆", default=False)
     is_school = fields.Boolean("是学校", default=False)
 
-    numberOfDevice = fields.Integer()
-    subordinateSchool = fields.One2many('school.school','eiec_id',readonly=True)
+    numberOfDevice = fields.Integer(string="设备数量")
+    subordinateSchool = fields.One2many('school.school','eiec_id',readonly=True,
+                                        string="下属学校")
 
-    test3Dtaoshu = fields.Integer()
-    testPLA = fields.Integer()
+    test3Dtaoshu = fields.Integer(string="3d设备套数")
+    testPLA = fields.Integer(string="线材购买数量")
